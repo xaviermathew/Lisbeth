955bb2690912566718db61c6908217a30db69e8c
Xavier Mathew <xavier@compile.com>
2020-08-23 03:33:15 +0530

    add url pattern to serve static files

--- lisbeth/core/urls.py
+++ lisbeth/core/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+import re
+
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^%s(?P<path>.*)$' % re.escape(settings.STATIC_URL.lstrip('/')),
+            serve,
+            kwargs={'document_root': settings.STATIC_ROOT}),
 ]
