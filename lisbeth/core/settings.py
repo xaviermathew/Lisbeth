db96c0e3509965dc911635ed0f2e9fcdf8390e57
Xavier Mathew <xavier@compile.com>
2020-08-23 03:27:14 +0530

    define STATIC_ROOT

--- lisbeth/core/settings.py
+++ lisbeth/core/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import warnings
+import os
 from pathlib import Path
+import warnings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # custom django settings
